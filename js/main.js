c305133f9a2dd8f4c397d0547658b9d95c30686b
Robert Junkins <67753581+JustBobinAround@users.noreply.github.com>
2022-12-15 18:54:29 -0500

    Removed Input handling and replaced with links

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
-
 var outcomeIndex = 0;
 var currentOutcome;
 var aquiredItems = ['NO_ITEM'];
 
-var input = document.getElementById('choice-input');
+var bottomAnchor = document.getElementById('bottom-anchor');
 var messages = document.getElementById('story-messages');
 var Story={};
 $.getJSON('./Story.json', function(data) { 
@@ -20,7 +19,7 @@ function printOutcome(){
   console.log('hit');
   var sb = '<hr><p>' + currentOutcome.info+'<br>';
   for(let i = 0; i < currentOutcome.choices.length; i++){
-    sb += '<br><b>' + i + '</b> : '+currentOutcome.choices[i].info;
+    sb += '<br><a onclick = "handleChoiceClick('+i+')"><b>' + i + '</b> : '+currentOutcome.choices[i].info+'</a>';
   }
   sb += '</p><br>';
 
@@ -30,38 +29,33 @@ function printWarning(text){
   var sb = '<p>' + text +'<br></p>';
   messages.innerHTML += sb;
 }
-//const Story = JSON.parse(jsonData);
-// Listen for the user to press enter in the input field
-input.addEventListener('keydown', function(event) {
-  if (event.keyCode === 13) {
-    // If the user pressed enter, get the value from the input field
-    var value = input.value;
-    updateOutcome();
-    checkChoice(value);
-    updateOutcome();
+function handleChoiceClick(choice){
+  updateOutcome();
+  checkChoice(choice);
+  updateOutcome();
+  
+  // Add the message to the chatbox
+  printOutcome();
+  bottomAnchor.scrollIntoView();
+}
 
-    // Clear the input field
-    input.value = '';
-    
-    // Add the message to the chatbox
-    printOutcome();
-    input.scrollIntoView();
-  }
-});
 function updateOutcome(){
   currentOutcome = Story.outcomes[outcomeIndex];
 }
 
 function checkChoice(choice){
   var unknownID = true;
+
   for(let i = 0; i < currentOutcome.choices.length; i++){
     if(currentOutcome.choices[i].id==choice){
+      printWarning('You entered <b>'+currentOutcome.choices[i].id+'</b>.');
       if(hasItem(currentOutcome.choices[i].requiredItem)){
         outcomeIndex = currentOutcome.choices[i].outcomePointer;
         if(hasItem(currentOutcome.choices[i].givenItem)==false){
           aquiredItems.push(currentOutcome.choices[i].givenItem);
-          printWarning('You aquired a '+currentOutcome.choices[i].givenItem);
+          printWarning('You aquired a <b>'+currentOutcome.choices[i].givenItem+'</b>');
         }
+
         i = currentOutcome.choices.length;
       }else{
         printWarning('You lack the required item.');
